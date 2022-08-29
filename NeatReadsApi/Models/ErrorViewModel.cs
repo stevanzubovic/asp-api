f80d17f13d57169e594bf341a49844a9305ef538
stevanzubovic <57267797+stevanzubovic@users.noreply.github.com>
2022-08-29 23:18:19 +0200

    removed file

--- NeatReadsApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NeatReadsApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
